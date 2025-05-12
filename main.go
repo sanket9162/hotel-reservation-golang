daf5a511c9720e2d116ecfbc59aa635094277b82
sanket9162 <sanketgondhali55@gmail.com>
2025-05-12 17:30:31 +0530

    add flag to listen address

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"flag"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
+	flag.Parse()
 
 	app := fiber.New()
+	apiv1 := app.Group("/api/v1")
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Listen(":3000")
+	apiv1.Get("/user", func(c *fiber.Ctx) error {
+		return c.SendString("user: sanket")
+	})
+
+	app.Listen(*listenAddr)
 
 }
