55480a58c1ebbc9bbb82fbd087747d154e752edb
sanket9162 <sanketgondhali55@gmail.com>
2025-05-12 19:29:30 +0530

    user types

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,9 +1,16 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sanket9162/hotel-reservation/types"
+)
 
 func HandleGetUsers(c *fiber.Ctx) error {
-	return c.JSON("gondhali")
+	u := types.User{
+		FirstName: "sanket",
+		LastName:  "Gondhali",
+	}
+	return c.JSON(u)
 }
 
 func HandleGetUser(c *fiber.Ctx) error {
