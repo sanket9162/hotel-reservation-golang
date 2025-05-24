45754b105c32e486b5c0a2fe9b8d3b528abe4263
sanket9162 <sanketgondhali55@gmail.com>
2025-05-24 12:29:00 +0530

    refactor seed and add seedUser

--- scripts/seed.go
+++ scripts/seed.go
@@ -12,22 +12,27 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
-	ctx := context.Background()
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
-	if err != nil {
-		log.Fatal(err)
-	}
-	hotelStore := db.NewMongoHotelStore(client)
-	roomStore := db.NewMongoRoomStore(client, hotelStore)
+var (
+	client     *mongo.Client
+	userStore  db.UserStore
+	hotelStore db.HotelStore
+	roomStore  db.RoomStore
+	ctx        = context.Background()
+)
 
+func seedHotel(name, location string) {
 	hotel := types.Hotel{
-		Name:     "Bellucia",
-		Location: "India",
-		Rating:   3,
+		Name:     name,
+		Location: location,
 		Rooms:    []primitive.ObjectID{},
 	}
 
+	insertedHotel, err := hotelStore.InserHotel(ctx, &hotel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("inserted hotel = ", insertedHotel)
 	rooms := []types.Room{
 		{
 			Size:  "small",
@@ -39,17 +44,53 @@ func main() {
 		},
 	}
 
-	insertedHotel, err := hotelStore.InserHotel(ctx, &hotel)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	for _, room := range rooms {
 		room.HotelID = insertedHotel.ID
 		insertedRoom, err := roomStore.InserRoom(ctx, &room)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(insertedRoom)
+		fmt.Println("inserted room = ", insertedRoom)
 	}
+}
+
+func seedUser(c context.Context, fname, lname, email, password string) {
+	user, err := types.NewUserFromParams(types.CreateUserParams{
+		FirstName: fname,
+		LastName:  lname,
+		Email:     email,
+		Password:  password,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := userStore.InsertUser(c, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted User = ", res)
+}
+
+func init() {
+	var err error
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI(db.DBURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.Database(db.DBNAME).Drop(ctx); err != nil {
+		log.Fatal(err)
+	}
+	userStore = db.NewMongoUserStore(client)
+	hotelStore = db.NewMongoHotelStore(client)
+	roomStore = db.NewMongoRoomStore(client, hotelStore)
+
+}
+
+func main() {
+	seedHotel("Bellucia", "France")
+	seedHotel("The cozy hotel", "The Netherlands")
+	seedHotel("Die another day", "UK")
+	seedUser(context.Background(), "James", "Foo", "james@foo.com", "supersecurepassword")
+	fmt.Println("seeded the db")
 }
