a497f7ca97b44c5428fa9411e1bc2e97ae13cc8e
sanket9162 <sanketgondhali55@gmail.com>
2025-05-24 12:35:35 +0530

    compare password

--- api/auth_handler.go
+++ api/auth_handler.go
@@ -1,10 +1,13 @@
 package api
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/sanket9162/hotel-reservation/db"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthHandler struct {
@@ -30,10 +33,17 @@ func (h *AuthHandler) HandleAuthenticate(c *fiber.Ctx) error {
 
 	user, err := h.userStore.GetUserByEmail(c.Context(), params.Email)
 	if err != nil {
-		return err
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return fmt.Errorf("invalid credentials")
+		}
 	}
 
-	fmt.Println(user)
+	err = bcrypt.CompareHashAndPassword([]byte(user.EncryptedPassword), []byte(params.Password))
+	if err != nil {
+		return fmt.Errorf("invalid cresentials")
+	}
+
+	fmt.Println("authenticated ->", user)
 
 	return nil
 }
