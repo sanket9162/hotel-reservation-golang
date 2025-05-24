86f575fc91897f11d104b59e163b7eb413a3e447
sanket9162 <sanketgondhali55@gmail.com>
2025-05-24 10:52:13 +0530

    parse token func

--- api/middleware/jwt.go
+++ api/middleware/jwt.go
@@ -2,8 +2,10 @@ package middleware
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func JWTAuthentication(c *fiber.Ctx) error {
@@ -14,7 +16,31 @@ func JWTAuthentication(c *fiber.Ctx) error {
 		return fmt.Errorf("unauthorized")
 	}
 
+	if err := parseToken(token); err != nil {
+		return nil
+	}
+
 	fmt.Println("token:", token)
 
 	return nil
+}
+
+func parseToken(tokenStr string) error {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			fmt.Println("invalid string method", token.Header["alg"])
+			return nil, fmt.Errorf("unauthorized")
+		}
+		secret := os.Getenv("JWT_SECRET")
+		return []byte(secret), nil
+	})
+	if err != nil {
+		fmt.Println("failed to parse JWT token:", err)
+		fmt.Errorf("unauthorized")
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println(claims)
+	}
+	return fmt.Errorf("unauthorized")
 }
