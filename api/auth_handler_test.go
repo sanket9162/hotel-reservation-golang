b0192deb835878966dda58d0ae143316659b074c
sanket9162 <sanketgondhali55@gmail.com>
2025-05-26 15:47:53 +0530

    insert Test User

--- api/auth_handler_test.go
+++ api/auth_handler_test.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -9,11 +10,31 @@ import (
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/sanket9162/hotel-reservation/db"
+	"github.com/sanket9162/hotel-reservation/types"
 )
+
+func insertTestUser(t *testing.T, userStore db.UserStore) *types.User {
+	user, err := types.NewUserFromParams(types.CreateUserParams{
+		FirstName: "james",
+		LastName:  "foo",
+		Email:     "james@foo.com",
+		Password:  "supersecurepassword",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = userStore.InsertUser(context.TODO(), user)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return user
+}
 
 func TestAuthenticate(t *testing.T) {
 	tdb := setup(t)
 	defer tdb.teardown(t)
+	insertTestUser(t, tdb.UserStore)
 
 	app := fiber.New()
 	authHandler := NewAuthHandler(tdb.UserStore)
@@ -26,6 +47,7 @@ func TestAuthenticate(t *testing.T) {
 
 	b, _ := json.Marshal(params)
 	req := httptest.NewRequest("POST", "/auth", bytes.NewReader(b))
+	req.Header.Add("Content-Type", "application/json")
 	resp, err := app.Test(req)
 	if err != nil {
 		t.Fatal(err)
