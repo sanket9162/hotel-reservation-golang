36c7950d92f0d5faeb883de66bcb955cd5919f51
sanket9162 <sanketgondhali55@gmail.com>
2025-05-29 11:19:31 +0530

    room booking handler

--- api/middleware/jwt.go
+++ api/middleware/jwt.go
@@ -7,24 +7,33 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/sanket9162/hotel-reservation/db"
 )
 
-func JWTAuthentication(c *fiber.Ctx) error {
-	token := c.Get("X-Api-Token")
-	if token == "" {
-		return fmt.Errorf("unauthorized")
+func JWTAuthentication(userStore db.UserStore) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		token := c.Get("X-Api-Token")
+		if token == "" {
+			return fmt.Errorf("unauthorized")
+		}
+		fmt.Println(token)
+		claims, err := validateToke(token)
+		if err != nil {
+			return err
+		}
+		expiresFloat := claims["expires"].(float64)
+		expires := int64(expiresFloat)
+		if time.Now().Unix() > expires {
+			return fmt.Errorf("token expired")
+		}
+		userID := claims["id"].(string)
+		user, err := userStore.GetUserById(c.Context(), userID)
+		if err != nil {
+			return fmt.Errorf("unaithorized")
+		}
+		c.Context().SetUserValue("user", user)
+		return c.Next()
 	}
-	fmt.Println(token)
-	claims, err := validateToke(token)
-	if err != nil {
-		return err
-	}
-	expiresFloat := claims["expires"].(float64)
-	expires := int64(expiresFloat)
-	if time.Now().Unix() > expires {
-		return fmt.Errorf("token expired")
-	}
-	return c.Next()
 }
 
 func validateToke(tokenStr string) (jwt.MapClaims, error) {
---
+++ api/room_handler.go
@@ -0,0 +1,55 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/sanket9162/hotel-reservation/db"
+	"github.com/sanket9162/hotel-reservation/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type RoomHandler struct {
+	store *db.Store
+}
+
+type BookRoomParams struct {
+	FromDate  time.Time `json:"fromDate"`
+	TillDate  time.Time `json:"tillDate"`
+	NumPerson int       `json:"numPerson"`
+}
+
+func NewRoomHandler(store *db.Store) *RoomHandler {
+	return &RoomHandler{
+		store: store,
+	}
+}
+
+func (h *RoomHandler) HandleBookRoom(c *fiber.Ctx) error {
+	var params BookRoomParams
+	if err := c.BodyParser(&params); err != nil {
+		return err
+	}
+	roomID, err := primitive.ObjectIDFromHex(c.Params("id"))
+	if err != nil {
+		return err
+	}
+	user, ok := c.Context().Value("user").(*types.User)
+	if !ok {
+		return c.Status(http.StatusInternalServerError).JSON(genericResp{
+			Type: "error",
+			Msg:  "internal server error",
+		})
+	}
+	booking := types.Booking{
+		UserID:    user.ID,
+		RoomID:    roomID,
+		FromDate:  params.FromDate,
+		TillDate:  params.TillDate,
+		NumPerson: params.NumPerson,
+	}
+	fmt.Println(booking)
+	return nil
+}
