e83380ea641c7bd1670843c09659255754dd8fd9
sanket9162 <sanketgondhali55@gmail.com>
2025-06-12 11:25:35 +0530

    updatebooking func  to cancel booking

--- db/booking_store.go
+++ db/booking_store.go
@@ -13,6 +13,7 @@ type BookingStore interface {
 	InsertBooking(context.Context, *types.Booking) (*types.Booking, error)
 	GetBookings(context.Context, bson.M) ([]*types.Booking, error)
 	GetBookingByID(context.Context, string) (*types.Booking, error)
+	UpdateBooking(context.Context, string, bson.M) error
 }
 
 type MongoBookingStore struct {
@@ -27,6 +28,16 @@ func NewBookingStore(client *mongo.Client) *MongoBookingStore {
 		client: client,
 		coll:   client.Database(DBNAME).Collection("bookings"),
 	}
+}
+
+func (s *MongoBookingStore) UpdateBooking(ctx context.Context, id string, update bson.M) error {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil
+	}
+	m := bson.M{"$set": update}
+	_, err = s.coll.UpdateByID(ctx, oid, m)
+	return err
 }
 
 func (s *MongoBookingStore) GetBookings(ctx context.Context, filter bson.M) ([]*types.Booking, error) {
